77e5dfa3b3a8ff3719d2242782d1c879c32ef8d5
Matheus Rebello <matheussrb@outlook.com>
2023-08-11 01:09:05 -0300

    Insert Data

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,42 +1,77 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import db.DB;
-import db.DbException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		Connection connection = null;
-		Statement statement = null;
-		ResultSet reSet = null;
-		
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
 		try {
+
+			// INITIALIZES CONNECTION AND STATEMENT
+			// EXAMPLE 1:
+			
 			connection = DB.getConnection();
+
+			ps = connection.prepareStatement("INSERT INTO seller "
+											+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) " 
+											+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
 			
-			statement = connection.createStatement();
-		
-			reSet = statement.executeQuery("SELECT * FROM department");
+			ps.setString(1, "Joao");
+			ps.setString(2, "joao@gmail.com");
+			ps.setDate(3, Date.valueOf(LocalDate.parse("04/08/2000", format)));
+			ps.setDouble(4, 1800.00);
+			ps.setInt(5, 3);
 			
-			while (reSet.next()) {
-				System.out.println(reSet.getInt("Id") + ", " + reSet.getString("Name"));
+			// EXAMPLE 2:
+						//st = conn.prepareStatement(
+						//		"INSERT INTO department (Name) values ('D1'),('D2')", 
+						//		Statement.RETURN_GENERATED_KEYS);
+
+			// THROWS A QUERY INTO DB AND SAVES THE NUMBERS OF ROWS AFFECTED IN QUERY
+
+			int rows_affected = ps.executeUpdate();
+			
+			// SHOW THE NEW INSERTED ID's
+
+			if (rows_affected > 0) {
+				rs = ps.getGeneratedKeys();
+
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("UPDATED! ID: " + id);
+				}
 			}
 			
-		} catch (Exception e) {
-			throw new DbException("### ERROR: " + e.getMessage());
-		}
+			else
+				System.out.println("No rows affected!");
 		
-		finally {
-			DB.closeResulSet(reSet);
-			DB.closeStatement(statement);
-			DB.closeConnection();
 		}
-		
 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		finally {
+			DB.closeResulSet(rs);
+			DB.closeStatement(ps);
+			DB.closeConnection();
+
+		}
 	}
 
 }
