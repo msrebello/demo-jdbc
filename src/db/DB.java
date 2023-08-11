edd14a4e66e894cb862d077a497ea3591a1cb7e1
Matheus Rebello <matheussrb@outlook.com>
2023-08-10 21:02:52 -0300

    Connection Methods and Class DbException

---
+++ src/db/DB.java
@@ -0,0 +1,63 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+	
+	// CONNECTION WITH DATABASE STARTS NULL:
+
+	private static Connection conn = null;
+
+	// AUXILIARY METHODS FOR CONNECTION TESTS:
+	
+	public static Connection getConnection() {
+
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url);
+
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+
+		return conn;
+	}
+	
+	private static void closeConnection() {
+
+		if (conn != null) {
+			try {
+				conn.close();
+
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	// LOADING DATABASE PROPERTIES:
+	
+	private static Properties loadProperties() {
+
+		try (FileInputStream file = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(file);
+
+			return props;
+		}
+
+		catch (IOException e) {
+
+			throw new DbException(e.getMessage());
+		}
+
+	}
+}
